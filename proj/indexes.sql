c770bb4cf29c03130162535dae4d91bcca224fa0
Rafał Laskowski <rafalek_0202@wp.pl>
2023-01-10 22:12:46 +0100

    fixed inedxes

--- proj/indexes.sql
+++ proj/indexes.sql
@@ -1,49 +1,50 @@
-CREATE NONCLUSTERED INDEX OrdersCustomerIDIndex ON Orders(CustomerID);
-CREATE NONCLUSTERED INDEX OrdersDatesIDIndex ON Orders(OrderDate,
-ReceiveDate);
+--Orders
+
+CREATE NONCLUSTERED INDEX OrdersDatesIDIndex on Orders(OrderDate, ReceiveDate);
+CREATE NONCLUSTERED INDEX ReadyForDelivery on Orders(IsPaid, TakeOut)
 
 ---
+--Reservations
 
-Create NONCLUSTERED INDEX ReservationCompanyIDIndex ON Reservations(CompanyID)
-Create NONCLUSTERED INDEX ReservationOrderIDIndex ON Reservations(OrderID)
-CREATE NONCLUSTERED INDEX ReservationDatesIDIndex ON Reservations(DoneReservationDate,
-ReservationDate);
-
----
-
-Create NONCLUSTERED INDEX CompanyNIPIndex ON CompanyCustomers(nip)
-
-
+Create NONCLUSTERED INDEX ReservationCompanyIDIndex on Reservations(CompanyID)
+Create NONCLUSTERED INDEX ReservationOrderIDIndex on Reservations(OrderID)
+CREATE NONCLUSTERED INDEX ReservationDatesIDIndex on Reservations(DoneReservationDate, ReservationDate);
 
 ---
+--Users
 
-
-CREATE NONCLUSTERED INDEX UserPhoneIndex on Users(PhoneNumber)
-CREATE NONCLUSTERED INDEX UserEmailIndex on Users(email)
 CREATE NONCLUSTERED INDEX UserNameIndex on Users(Name)
 
-
---
+---
+--Categories
 
 CREATE NONCLUSTERED INDEX CategoryNameIndex on Categories(Name)
 
---
+---
+--Products
 
 CREATE NONCLUSTERED INDEX ProductNameIndex on Products(Name)
+CREATE NONCLUSTERED INDEX ProductNameIndex on Products(CategoryID)
+CREATE NONCLUSTERED INDEX WasUsedBeforeIndex on Products(IsActive)
 
---
+---
+--Menus
 
 CREATE NONCLUSTERED INDEX MenuDatesIDIndex on Menus(InDate, OutDate)
 CREATE NONCLUSTERED INDEX MenuDetailsProductIDIndex on MenuDetails(ProductID)
 
-
---
+---
+-- Discounts/DiscountConditions/DiscountParams
 
 CREATE NONCLUSTERED INDEX DiscountConditionsHistoryIndex on DiscountConditions(InDate, OutDate)
 CREATE NONCLUSTERED INDEX DiscountParamsHistoryIndex on DiscountParams(InDate, OutDate)
+CREATE NONCLUSTERED INDEX DiscountUsedIndex on Discounts(UsedDate, EndDate)
+CREATE NONCLUSTERED INDEX DiscountNameIndex on Discounts(DiscountType)
 
---
+---
+--Employees
 
-CREATE NONCLUSTERED INDEX CityNameIndex on Cities(Name)
+CREATE NONCLUSTERED INDEX HireDateIndex on Employees(HireDate)
+CREATE NONCLUSTERED INDEX PositionIndex on Employees(Position)
 
 
